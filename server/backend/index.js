b1c0f6e880b0afe07a662b43a7402aaeedba6a26
tibssy <norbert.tiborcz@gmail.com>
2023-02-07 11:19:47 +0000

    no-irregular-whitespace fixed

--- server/backend/index.js
+++ server/backend/index.js
@@ -5,7 +5,8 @@ const app = new express();
 //Adding a middleware to set the path for all static pages
 app.use(express.static('pages'))
 
-const PORT = process.env.PORT || 8000; 
+const PORT = process.env.PORT || 8000;
+// 
 
 app.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`)
